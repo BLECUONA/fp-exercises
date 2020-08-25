327b2181e986ec683b1a3b59249bbebe5c096bb2
BLECUONA <benjaminlecuona@gmail.com>
2020-08-25 09:18:44 +0200

    ex1_depth : v1

--- deepArrayInfos.ts
+++ deepArrayInfos.ts
@@ -1,4 +1,5 @@
 type DeepNumberArray = number | DeepNumberArray[];
+
 const sum = (input: DeepNumberArray) => {
   let res = 0;
   if (Array.isArray(input)) {
@@ -11,6 +12,47 @@ const sum = (input: DeepNumberArray) => {
   }
 };
 
+const everyDepth = (input: any[]) => {
+  let res = 0;
+
+  input.forEach((element) => {
+    let increaseDepth = memoizeDepth();
+
+    const elementDepth = depth(element, increaseDepth);
+    if (elementDepth > res) res = elementDepth;
+  });
+
+  return res;
+};
+
+const depth = (
+  input: DeepNumberArray,
+  memo: (input: DeepNumberArray) => number
+) => {
+  if (Array.isArray(input)) {
+    let deep = 0;
+    input.forEach((element) => {
+      memo(element);
+      const temp = depth(element, memo);
+      if (temp > deep) deep = temp;
+    });
+    return deep;
+  } else return memo(input);
+};
+
+const memoizeDepth = () => {
+  let depthRes = 0;
+
+  return (input: DeepNumberArray) =>
+    Array.isArray(input) ? (depthRes += 1) : depthRes;
+};
+
 const array = [1, [[2], 3], [4], 5, [6, 42, [[86], [[12]], 1337]], 1];
-const result = sum(array);
-console.log(`Sum is ${result}`);
+
+// sum
+const sumResult = sum(array);
+console.log(`Sum is ${sumResult}`);
+
+// depth
+const depthResult = everyDepth(array);
+console.log(`Depth is ${depthResult}`);
